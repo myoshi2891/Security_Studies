722b158ec042c0868b3f9a611be25cbf12aa52b0
myoshizumi <myoshizumi@alumni.berklee.edu>
2026-03-27 22:31:54 +0900

    docs: rename 01.html and start tracking html directory

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+// prettier.config.cjs
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 4,
+  useTabs: false,
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
